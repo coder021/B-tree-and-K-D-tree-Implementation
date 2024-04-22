a4994c99f4c81054516fed5f94c653b839dda680
Supritha-Kamalanathan <131264610+Supritha-Kamalanathan@users.noreply.github.com>
2024-04-22 11:48:13 +0530

    Update ProgramCode.cpp

--- ProgramCode.cpp
+++ ProgramCode.cpp
@@ -4,24 +4,64 @@
 
 class BTree
 {
-  struct node
+private:
+    int MAX_KEYS;
+    int MAX_CHILDREN;
+    struct node
+    {
+        int n; // no. of keys in the node
+        int key[MAX_KEYS]; // 
+        node *child[MAX_CHILDREN];
+        bool leaf;
+    };
 
 public:
-  int insert();
-  int del();
-  int search(int );
-  
-}
+    BTree(int keys, int children)
+    {
+        MAX_KEYS = keys;
+        MAX_CHILDREN = children;
+    }
+
+    int insert();
+    int del();
+    int callSearch(int);
+    struct node *search(struct node *, int);
+
+    ~BTree()
+    {
+
+    }
+};
 
 int main(void)
 {
-  
+    BTree b;
+    int btree_order;
+    printf("Enter the order of B Tree: ");
+    scanf("%d", &btree_order);
+
 }
 
-// Method to insert into a b tree - Time complexities O(n)
+// Method to insert into a b tree
 
-// Method to delete in a b tree - Time complexities O(n)
+// Method to delete in a b tree
 
-// Method to search in a b tree - Time complexities O(n)
+// Method to call the search method
+int BTree::callSearch(int num)
+{
+    if (search(root, num) == NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// Method to search in a b tree
+BTree::node *BTree::search(struct node *temp, int num)
+{
+}
 
 // Method to display b tree
