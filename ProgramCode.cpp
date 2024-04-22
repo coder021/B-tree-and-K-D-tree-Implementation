aa2f4182905c509a9c3c9c2edd76c5dcc1b3bd2a
Supritha-Kamalanathan <131264610+Supritha-Kamalanathan@users.noreply.github.com>
2024-04-22 11:09:26 +0530

    Update ProgramCode.cpp

--- ProgramCode.cpp
+++ ProgramCode.cpp
@@ -2,14 +2,25 @@
 
 #include <stdio.h>
 
+class BTree
+{
+  struct node
+
+public:
+  int insert();
+  int del();
+  int search(int );
+  
+}
+
 int main(void)
 {
   
 }
 
-// Method to insert into a b treeTime complexities O(n)
+// Method to insert into a b tree - Time complexities O(n)
 
-// Method to delete in a b tree-Time complexities O(n)
+// Method to delete in a b tree - Time complexities O(n)
 
 // Method to search in a b tree - Time complexities O(n)
 
