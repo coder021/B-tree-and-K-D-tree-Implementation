007019faa364e9691e12a6eb49f65f9a5bc8c19e
Supritha-Kamalanathan <131264610+Supritha-Kamalanathan@users.noreply.github.com>
2024-04-23 19:33:39 +0530

    Update ProgramCode.cpp

--- ProgramCode.cpp
+++ ProgramCode.cpp
@@ -15,23 +15,27 @@ private:
         struct node **child;
         bool leaf;
     };
-    node* m_root;
+    struct node *m_root = (struct node *)malloc(sizeof(struct node));
 
 public:
     BTree(int keys, int children)
     {
         MAX_KEYS = keys;
         MAX_CHILDREN = children;
-        m_root->key = (int *)malloc(MAX_KEYS * sizeof(int));
-        m_root->child = (struct node **)malloc(MAX_CHILDREN * sizeof(struct node));
+        // m_root->key = (int *)malloc((2 * MAX_CHILDREN - 1) * sizeof(int));
+        // m_root->child = (struct node **)malloc((2 * MAX_CHILDREN) * sizeof(struct node *));
+        m_root = NULL;
     }
 
-    int insert();
+    struct node *createNode(bool);
+    void insertNonFull(struct node *, int);
+    void insert(int);
+    void splitChild(int, struct node *, struct node *);
     int del();
     int callSearch(int);
     struct node *search(struct node *, int);
     void callDisplay();
-    void display(struct node*);
+    void display(struct node *);
 
     ~BTree()
     {
@@ -44,44 +48,160 @@ int main(void)
     printf("Enter the order of B Tree: ");
     scanf("%d", &btree_order);
     BTree b(btree_order - 1, btree_order);
-    int ch = 0;
+    int ch = 0, num;
     while (ch != 5)
     {
         printf("\nMenu:\n1. Insert\n2. Delete\n3. Search\n4. Display\n5. Exit\n\nEnter your choice: ");
         scanf("%d", &ch);
         switch (ch)
         {
-            case 1:
-            {
-                break;
-            }
-            case 2:
-            {
-                break;
-            }
-            case 3:
-            {
-                break;
-            }
-            case 4:
-            {
-                b.callDisplay();
-                break;
-            }
-            case 5:
-            {
-                printf("Exiting... Thank you!\n");
-                break;
-            }
-            default:
-            {
-                printf("Enter valid choice!\n");
-            }
+        case 1:
+        {
+            printf("Enter number to insert: ");
+            scanf("%d", &num);
+            b.insert(num);
+            break;
+        }
+        case 2:
+        {
+            break;
+        }
+        case 3:
+        {
+            
+            break;
+        }
+        case 4:
+        {
+            b.callDisplay();
+            break;
+        }
+        case 5:
+        {
+            printf("Exiting... Thank you!\n");
+            break;
+        }
+        default:
+        {
+            printf("Enter valid choice!\n");
+        }
         }
     }
 }
 
-// Method to insert into a b tree
+// Method to create a new node
+BTree::node *BTree::createNode(bool isleaf)
+{
+    struct node *newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->key = (int *)malloc((2 * MAX_CHILDREN - 1) * sizeof(int));
+    newnode->child = (struct node **)malloc((2 * MAX_CHILDREN) * sizeof(struct node *));
+    newnode->n = 0;
+    newnode->leaf = isleaf;
+    return newnode;
+}
+
+// Method to insert a node into a non full b tree
+void BTree::insertNonFull(struct node *node, int num)
+{
+    int i = node->n - 1;
+    if (node->leaf == true)
+    {
+        while (i >= 0 && node->key[i] > num)
+        {
+            node->key[i + 1] = node->key[i];
+            i--;
+        }
+
+        node->key[i + 1] = num;
+        node->n = node->n + 1;
+    }
+    else
+    {
+        while (i >= 0 && node->key[i] > num)
+        {
+            i--;
+        }
+        if (node->child[i + 1]->n == 2 * MAX_CHILDREN - 1)
+        {
+            splitChild(i + 1, node->child[i + 1], node);
+
+            if (node->key[i + 1] < num)
+            {
+                i++;
+            }
+        }
+
+        insertNonFull(node->child[i + 1], num);
+    }
+}
+
+// Method to split a node
+void BTree::splitChild(int i, struct node *node, struct node *newnode)
+{
+    struct node *lastNode = createNode(node->leaf);
+    lastNode->n = MAX_CHILDREN - 1;
+
+    for (int j = 0; j < MAX_CHILDREN - 1; j++)
+    {
+        lastNode->key[j] = node->key[j + MAX_CHILDREN];
+    }
+    if (node->leaf == false)
+    {
+        for (int j = 0; j < MAX_CHILDREN; j++)
+        {
+            lastNode->child[j] = node->child[j + MAX_CHILDREN];
+        }
+    }
+    node->n = MAX_CHILDREN - 1;
+
+    for (int j = newnode->n; j >= i + 1; j--)
+    {
+        newnode->child[j + 1] = newnode->child[j];
+    }
+    newnode->child[i + 1] = lastNode;
+
+    for (int j = node->n - 1; j >= i; j--)
+    {
+        newnode->key[j + 1] = newnode->key[j];
+    }
+    newnode->key[i] = node->key[MAX_CHILDREN - 1];
+    newnode->n = newnode->n + 1;
+}
+
+// Method to insert a node into a b tree
+void BTree::insert(int num)
+{
+    if (m_root == NULL)
+    {
+        struct node *newnode = createNode(true);
+        newnode->key[0] = num;
+        newnode->n = 1;
+        m_root = newnode;
+    }
+    else
+    {
+        if (m_root->n == 2 * MAX_CHILDREN - 1)
+        {
+            struct node *newnode = createNode(false);
+            newnode->child[0] = m_root;
+
+            splitChild(0, m_root, newnode);
+
+            int i = 0;
+            if (newnode->key[0] < num)
+            {
+                i++;
+            }
+
+            insertNonFull(newnode->child[i], num);
+            m_root = newnode;
+        }
+        else
+        {
+            insertNonFull(m_root, num);
+        }
+    }
+}
 
 // Method to delete in a b tree
 
@@ -124,9 +244,14 @@ void BTree::callDisplay()
 }
 
 // Method to display b tree
-void BTree::display(struct node* temp)
+void BTree::display(struct node *temp)
 {
     int i;
+    if (m_root == NULL)
+    {
+        printf("B Tree is empty!\n");
+        return;
+    }
     for (i = 0; i < temp->n; i++)
     {
         if (temp->leaf == false)
